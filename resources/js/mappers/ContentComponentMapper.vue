184dcb57e1ca6d71147ecf3bcd6c17dd18621a53
eatthatpie <tomek.pugn@gmail.com>
2018-09-12 20:33:41 +0200

    add basic components mappers

---
+++ resources/js/mappers/ContentComponentMapper.vue
@@ -0,0 +1,19 @@
+<template>
+    <content-component :body="data.body" />
+</template>
+
+<script>
+import ContentComponent from './../components/ContentComponent'
+
+export default {
+    components: {
+        ContentComponent
+    },
+    props: {
+        data: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
