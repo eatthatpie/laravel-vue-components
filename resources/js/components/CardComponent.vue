352de21c6d2cabe3b0678cf737568105ce66ca22
eatthatpie <tomek.pugn@gmail.com>
2018-09-12 20:33:12 +0200

    add basic components

---
+++ resources/js/components/CardComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card">
+        <div class="card-image">
+            <figure class="image is-4by3">
+                <img :src="image" :alt="alt" />
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="content" v-html="body" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        image: {
+            type: String,
+            required: true
+        },
+        alt: {
+            type: String,
+            required: true
+        },
+        body: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
---
+++ resources/js/components/ContentComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="content" v-html="body" />
+</template>
+
+<script>
+export default {
+    props: {
+        body: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
